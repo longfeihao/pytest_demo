459cc8e7bc2f716898a0daef3e66ecbd9f5e4d32
longfeihao <hlf.hao@qq.com>
2023-06-04 16:48:53 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pytest_demo
+
+
+本项目用于验证pytest的基本使用方法。
+
---
+++ tests/stack_test.py
@@ -0,0 +1,38 @@
+import pytest
+
+from src.stack import Stack
+
+
+@pytest.fixture(name="stack_inst")
+def stack_fixture():
+    return Stack()
+
+
+@pytest.mark.parametrize('passwd',
+                      ['abcdefdfs',
+                       'as52345fasdf4'])
+def passwd_length_test(passwd):
+    assert len(passwd) >= 8
+
+
+def constructor_test(stack_inst):
+    assert isinstance(stack_inst, Stack)
+    assert len(stack_inst) == 0
+
+
+def push_test(stack_inst):
+    stack_inst.push(3)
+    assert len(stack_inst) == 1
+    stack_inst.push(5)
+    assert len(stack_inst) == 2
+
+
+def pop_test(stack_inst):
+    stack_inst.push("abc")
+    stack_inst.push("def")
+    assert stack_inst.pop() == "def"
+    assert len(stack_inst) == 1
+    assert stack_inst.pop() == "abc"
+    assert len(stack_inst) == 0
+    with pytest.raises(IndexError, match=r".*stack is empty.*"):
+        stack_inst.pop()
